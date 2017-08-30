c6968b21fdc7aec01789a3fe0a48e858020a62ed
willischan <willischan87@gmail.com>
2017-08-29 19:32:21 -0700

    Added all products link and added bootstrap

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,10 @@ class ProductsController < ApplicationController
     @first_product = Product.first
     render "first_product.html.erb"
   end
+
+  def all_products_method
+    @all_products = Product.all
+    render "all_products.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/first_product" => "products#first_product_method"
+  get "/all_products" => "products#all_products_method"
 end
