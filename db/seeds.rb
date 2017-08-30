6bb677b6b386379b830504615c56c5423b295f6b
willischan <willischan87@gmail.com>
2017-08-29 19:58:06 -0700

    Tested rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+item = Product.create(
+  [
+  name:"Samsung Galaxy S8",
+  price:800,
+  image:"https://cnet4.cbsistatic.com/img/AVSpk30o9vxRTG-eEgKHmUCbG3I=/830x467/2017/03/27/a4184fe4-5fa0-4a57-b0b2-3bb5601ef536/gsocho-ny-19-jg.jpg",
+  description:"The alternative to iPhones"]
+  )
