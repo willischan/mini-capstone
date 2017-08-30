48b4a5232aeb501a9bc2167d915bb2cdaef12785
willischan <willischan87@gmail.com>
2017-08-29 19:18:10 -0700

    Adding first_product file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def first_product_method
+    @first_product = Product.first
+    render "first_product.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/first_product" => "products#first_product_method"
 end
